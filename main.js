dd372d5564d7db3705a33c1a442f4c75f2f82c13
Ramo <ramoalikulov77@gmail.com>
2024-10-19 19:05:53 +0300

    update project

---
+++ main.js
@@ -0,0 +1,56 @@
+/** @format */
+
+const inputEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
+const timerEl = document.querySelector("span");
+
+const createTimerAnimator = () => {
+  return (seconds) => {
+    const interval = setInterval(() => {
+      seconds--;
+
+      printTime(seconds);
+
+      if (seconds === 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  };
+};
+
+function printTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+
+  const minutes = Math.floor((seconds - hours * 3600) / 60);
+
+  const newSeconds = seconds % 60;
+
+  timerEl.innerHTML = `
+  ${hours.toString().padStart(2, "0")}:
+  ${minutes.toString().padStart(2, "0")}:
+  ${newSeconds.toString().padStart(2, "0")}
+  `;
+}
+
+const animateTimer = createTimerAnimator();
+
+inputEl.addEventListener("input", (e) => {
+  const str = e.target.value;
+
+  e.target.value = str.replace(/\D/g, "");
+});
+
+buttonEl.addEventListener("click", (e) => {
+  if (inputEl.value == "") return;
+
+  const seconds = Number(inputEl.value);
+
+  animateTimer(seconds);
+
+  inputEl.value = `${inputEl.value}seconds`;
+  inputEl.setAttribute("disabled", true);
+  inputEl.setAttribute("class", "disabled-input");
+
+  e.target.setAttribute("disabled", true);
+  e.target.setAttribute("class", "disabled-button");
+});
